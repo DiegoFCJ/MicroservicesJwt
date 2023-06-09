ac440209152ee0746e012ce1d26ae10bf2c4db74
DiegoFCJ <103056792+DiegoFCJ@users.noreply.github.com>
2023-06-09 14:31:48 +0200

    fix

---
+++ shop/src/main/java/com/microJwt/shop/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.microJwt.shop.controller;
+
+import com.microJwt.shop.dto.ProductRequest;
+import com.microJwt.shop.dto.ProductResponse;
+import com.microJwt.shop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProd(@RequestBody ProductRequest productRequest){
+        productService.createProd(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProds(){
+        return productService.getallProd();
+    }
+}
